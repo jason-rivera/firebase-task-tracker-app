7803ae7ce9d57693d5ad93da688c54831cfb6e7b
Jason Rivera <jp_rivera_@hotmail.com>
2021-02-28 23:03:56 -0800

    first commit

---
+++ firebase.js
@@ -0,0 +1,12 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAaPnXyaQfkvde57Prfk-DI6esprZkLWHE",
+    authDomain: "tasktrackerapp-e0d1c.firebaseapp.com",
+    projectId: "tasktrackerapp-e0d1c",
+    storageBucket: "tasktrackerapp-e0d1c.appspot.com",
+    messagingSenderId: "519931259334",
+    appId: "1:519931259334:web:177bb54da410062261aac7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
