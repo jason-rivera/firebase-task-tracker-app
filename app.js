87fc2ab9238f36b1ad4dc2f4407e6657d7f89bdd
Jason Rivera <jp_rivera_@hotmail.com>
2021-03-02 00:25:48 -0800

    Updated files

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
 const taskList = document.querySelector('#task-list');
 const form = document.querySelector('#add-task-form');
 
+/* create element and render tasks */
 function renderTaskList(doc) {
     let li = document.createElement('li');
     let task = document.createElement('span');
+    let cross = document.createElement('span');
 
     li.setAttribute('data-id', doc.id);
     task.textContent = doc.data().task;
+    cross.textContent = 'x';
+    cross.className = 'cross';
 
     li.appendChild(task);
+    li.appendChild(cross);
     taskList.appendChild(li);
+
+    /* deleting data */
+    cross.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('todolist').doc(id).delete();
+    })
 }
 
+/* saving data */
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     db.collection('todolist').add({
         task: form.task.value,
-        author: form.author.value,
-        category: form.category.value
     });
     form.task.value = '';
-    form.author.value = '';
-    form.category.value = '';
     console.log('Task added!');
 })
 
-db.collection('todolist').orderBy('task').onSnapshot(snapshot => {
+/* real-time listener */
+db.collection('todolist').onSnapshot(snapshot => {
     let changes = snapshot.docChanges();
     changes.forEach(change => {
         if (change.type == 'added') {
